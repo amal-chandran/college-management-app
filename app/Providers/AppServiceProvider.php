4f431727d7d2e2622afb83967b0e90947c64f9ef
ADev <amalchandrandev@gmail.com>
2018-08-06 19:04:49 +0530

    added artisan view generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,8 +28,8 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
         }
 
-        // if ($this->app->environment() == 'local') {
-        //     $this->app->register("Sven\ArtisanView\ArtisanViewServiceProvider");
-        // }
+        if ($this->app->environment() == 'local') {
+            $this->app->register("Sven\ArtisanView\ArtisanViewServiceProvider");
+        }
     }
 }
