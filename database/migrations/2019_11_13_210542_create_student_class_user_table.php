7e5f655f32f0fda4fd8e07e23215b0d1560fe1d2
Amal Chandran <amalchandrandev@gmail.com>
2019-09-23 14:02:16 +0530

    Seader problem resolved

--- database/migrations/2019_11_13_210542_create_student_class_user_table.php
+++ database/migrations/2019_11_13_210542_create_student_class_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateStudentClassUsersTable extends Migration
+class CreateStudentClassUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateStudentClassUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('student_class_user', function(Blueprint $table)
-        {
+        Schema::create('student_class_user', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->integer('student_class_id')->unsigned()->nullable()->index();
             $table->integer('user_id')->unsigned()->nullable()->index();
-
         });
     }
 
