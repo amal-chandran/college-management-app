49bc6b6e4b717e4c420306c9e3bbf113ad9c56ee
Amal Chandran <amalchandrandev@gmail.com>
2019-11-26 10:50:31 +0530

    Removed search and contact

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -38,22 +38,7 @@
             <span class="fas fa-home" aria-hidden="true"></span> Home
           </a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <a href="#" class="nav-link">Contact</a>
-        </li>
       </ul>
-
-      <!-- SEARCH FORM -->
-      <form class="form-inline ml-3">
-        <div class="input-group input-group-sm">
-          <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-navbar" type="submit">
-              <i class="fas fa-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
 
       <!-- Right navbar links -->
       <ul class="navbar-nav ml-auto">
